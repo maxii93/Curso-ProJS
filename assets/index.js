8eab0d70bf6af80b6ff7c3c7b0932496a0452abb
Maximiliano Fiorina <maxf993@gmail.com>
2019-12-11 21:53:42 -0300

    Primer Plugin del curso (autoplay) y boton de mute funcionando

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js'
+
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+const btnmute = document.querySelector(".mutear")
+
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+
+button.onclick = () => player.togglePlay();
+
+btnmute.onclick = () => player.toggleMute();
